cc2b11c2043c426917108657c59f7ddb2279af74
yamato0211 <kumayama0211@gmail.com>
2022-10-22 06:06:36 +0900

    [add] frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Todos } from './components/Todos';
+
+
+
+function App() { 
+  return (
+    <div className="App">
+      <h1>Todo App with Golang</h1>
+      <Todos/>
+    </div>
+  );
+}
+
+export default App;
